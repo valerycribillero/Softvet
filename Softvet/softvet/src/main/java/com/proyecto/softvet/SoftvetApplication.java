9fd4eea8ddef22a0376299321b1b410ab36d5ed9
vale <va.cribillero@duocuc.cl>
2026-05-08 14:59:06 -0400

    proyecto

---
+++ Softvet/softvet/src/main/java/com/proyecto/softvet/SoftvetApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.softvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftvetApplication.class, args);
+	}
+
+}
---
+++ Softvet/softvet/src/test/java/com/proyecto/softvet/SoftvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.softvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
